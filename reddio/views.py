044bf8b6379a90ae789e458a60edf1f7902b7cb8
pradeepvarma22 <pradeepnadimpalli22@gmail.com>
2022-12-01 02:35:05 +0530

    add: basic view
    Signed-off-by: pradeepvarma22 <pradeepnadimpalli22@gmail.com>

--- reddio/views.py
+++ reddio/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
-
+from reddio.models import User, Contract, ContractTxns
 # Create your views here.
 
 
 def home(request):
+    # mydata = Members.objects.all().order_by('-firstname').values()
+    # get latest 10 txns
+    # get 10 users
+    # get 10 contracts list
 
-    return render(request, "reddio/index.html")
+    txns = ContractTxns.objects.all().order_by('-timestamp').values()[:15]
+    accounts = User.objects.all().values()[:15]
+    contracts = Contract.objects.all().values()[:10]
+
+    context = {
+        'latestTxns': txns,
+        'userAccounts':accounts,
+        'contracts':contracts
+    }
+
+
+
+    return render(request, "reddio/index.html",context)
     
