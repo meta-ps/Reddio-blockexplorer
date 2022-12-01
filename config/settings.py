290e401cb5cce59c046137c15ed7114cdbb1fee1
pradeepvarma22 <pradeepnadimpalli22@gmail.com>
2022-12-01 11:03:09 +0530

    prod env

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-!453d48=7!&dl!^^68$-%c4j2dewtu&wh@nr)i+=dd1mh**v4a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://reddioexplorer.pythonanywhere.com/','reddioexplorer.pythonanywhere.com', '*']
 
 
 # Application definition
